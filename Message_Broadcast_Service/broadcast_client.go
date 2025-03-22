873df527416c652ba92cf1bb557e98998e2a213d
Daniel Tujuma <danielsevani12@gmail.com>
2025-03-22 13:42:33 +0300

    add comments

--- Message_Broadcast_Service/broadcast_client.go
+++ Message_Broadcast_Service/broadcast_client.go
@@ -1,3 +1,4 @@
+// client to listen and send to the broadcast server
 package main
 
 import (
@@ -9,14 +10,18 @@ import (
 )
 
 func main() {
+	// connect the client to the broadcast server
 	conn, err := net.Dial("tcp", "localhost:8080")
 	if err != nil {
 		fmt.Println("Connection error: ", err)
 		return
 	}
+	// close the connection after finishing
 	defer conn.Close()
 
+	// listen from the server message
 	go func() {
+		// read from the connection
 		reader := bufio.NewReader(conn)
 		for {
 			msg, err := reader.ReadString('\n')
@@ -29,14 +34,17 @@ func main() {
 		}
 	}()
 
+	// start reader to read from the standar input
 	inputReader := bufio.NewReader(os.Stdin)
 
 	for {
 		fmt.Print("$ ")
 		input, _ := inputReader.ReadString('\n')
 
+		// clean unnecessary characters
 		msg := strings.TrimSpace(input)
 
+		// send to the server
 		_, err := conn.Write([]byte(msg + "\n"))
 
 		if msg == "exit" {

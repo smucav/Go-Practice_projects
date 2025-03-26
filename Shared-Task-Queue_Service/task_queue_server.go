9dd91d426a05f3c43baeb197a2df1333f4a3f9e1
Daniel Tujuma <danielsevani12@gmail.com>
2025-03-26 23:32:53 +0300

    fix tcp connection bug

--- Shared-Task-Queue_Service/task_queue_server.go
+++ Shared-Task-Queue_Service/task_queue_server.go
@@ -9,64 +9,80 @@ import (
 )
 
 type setTask struct {
-	task   string
-	respCh chan bool
+	task string
 }
 
-type getTask struct {
-	respCh chan string
+type getTask struct{}
+
+type client struct {
+	conn    net.Conn
+	setTask setTask
+	getTask getTask
 }
 
 type TaskQueueServer struct {
-	gets    chan getTask
-	sets    chan setTask
+	gets    chan client
+	sets    chan client
 	closeCh chan struct{}
 	wg      sync.WaitGroup
 }
 
 func NewTaskQueueServer() *TaskQueueServer {
 	return &TaskQueueServer{
-		gets:    make(chan getTask),
-		sets:    make(chan setTask),
+		gets:    make(chan client),
+		sets:    make(chan client),
 		closeCh: make(chan struct{}),
 	}
 }
 
 func (tqs *TaskQueueServer) Run() {
-
 	ln, err := net.Listen("tcp", ":8080")
 	if err != nil {
-		fmt.Println("connection error")
+		fmt.Println("Server Failed")
 		return
 	}
-
-	for {
-		conn, err := ln.Accept()
-		if err != nil {
-			fmt.Println("Connection error")
-			return
-		}
-		tqs.wg.Add(1)
-		go tqs.handleClient(conn)
-	}
+	defer ln.Close()
+	fmt.Println("Server started on :8080")
 
 	tqs.wg.Add(1)
 	go func() {
 		defer tqs.wg.Done()
-		queue := make([]string, 0)
+		for {
+			conn, err := ln.Accept()
+			if err != nil {
+				fmt.Println("Connection error")
+				return
+			}
+			tqs.wg.Add(1)
+			go tqs.handleClient(conn)
+		}
+	}()
+
+	go func() {
+		var queue []string
 		for {
 			select {
-			case get := <-tqs.gets:
-				get.respCh <- queue[0]
-				queue = queue[1:]
-			case set := <-tqs.sets:
-				queue = append(queue, set.task)
-				set.respCh <- true
+			case getc := <-tqs.gets:
+				if len(queue) == 0 {
+					fmt.Fprintf(getc.conn, "There is No task have fun.")
+				} else {
+					fmt.Printf("Task %s Taken\n", queue[0])
+					fmt.Fprintf(getc.conn, "Task -> "+queue[0]+".")
+					queue = queue[1:]
+				}
+
+			case setc := <-tqs.sets:
+				queue = append(queue, setc.setTask.task)
+				fmt.Printf("Task %s Added\n", queue[len(queue)-1])
+				fmt.Fprintf(setc.conn, "Task added successfully.")
 			case <-tqs.closeCh:
 				return
 			}
 		}
 	}()
+
+	tqs.wg.Wait()
+	close(tqs.closeCh)
 }
 
 func (tqs *TaskQueueServer) handleClient(conn net.Conn) {
@@ -74,6 +90,7 @@ func (tqs *TaskQueueServer) handleClient(conn net.Conn) {
 	defer conn.Close()
 
 	reader := bufio.NewReader(conn)
+	fmt.Fprintf(conn, "Welcome to task queue \nAdd task(Task: task you want to add) or get(NEXT).")
 
 	for {
 		input, err := reader.ReadString('\n')
@@ -84,41 +101,35 @@ func (tqs *TaskQueueServer) handleClient(conn net.Conn) {
 		msg := strings.TrimSpace(input)
 
 		if msg == "exit" {
-			close(tqs.closeCh)
-			return
+			fmt.Fprintf(conn, "Good bye :)\n")
+			continue
 		}
 
 		if msg == "NEXT" {
-			get := getTask{respCh: make(chan string)}
+			get := client{
+				conn:    conn,
+				getTask: getTask{},
+			}
 			tqs.gets <- get
+			continue
 		}
 
-		if strings.HasPrefix(msg, "Task") {
-			task := strings.TrimSpace(strings.TrimPrefix(msg, "Task: "))
+		if strings.HasPrefix(strings.ToLower(msg), "task") {
+			task := strings.TrimSpace(strings.TrimPrefix(strings.ToLower(msg), "task: "))
 			if task == "" {
 				fmt.Fprintf(conn, "Invalid Page\n")
 
 				continue
 			}
-			set := setTask{task: task, respCh: make(chan bool)}
-			tqs.sets <- set
-		}
-	}
-	tqs.wg.Add(1)
-	go func() {
-		defer tqs.wg.Done()
-		for {
-			select {
-			case t := <-tqs.gets:
-				task := <-t.respCh
-				fmt.Fprintf(conn, task)
-			case <-tqs.sets:
-				fmt.Fprintf(conn, "Task set successfully\n")
-			case <-tqs.closeCh:
-				return
+			set := client{
+				conn:    conn,
+				setTask: setTask{task: task},
 			}
+			tqs.sets <- set
+			continue
 		}
-	}()
+		fmt.Fprintf(conn, "Unknown command.")
+	}
 }
 
 func main() {

67fa776f2861cd56ab8d4782cae8adf6413c2a02
Daniel Tujuma <danielsevani12@gmail.com>
2025-03-26 23:33:20 +0300

    fix listen connection bug

--- Shared-Task-Queue_Service/task_queue_client.go
+++ Shared-Task-Queue_Service/task_queue_client.go
@@ -6,27 +6,38 @@ import (
 	"net"
 	"os"
 	"strings"
+	"sync"
 )
 
 func main() {
 	conn, err := net.Dial("tcp", "localhost:8080")
 
 	if err != nil {
-		fmt.Println("Can't connect to server")
+		fmt.Println("Connection error: ", err)
 		return
 	}
 
-	reader := bufio.NewReader(conn)
+	defer conn.Close()
 
+	closeCh := make(chan struct{})
+	var wg sync.WaitGroup
+
+	wg.Add(1)
 	go func() {
+		defer wg.Done()
+		reader := bufio.NewReader(conn)
 		for {
-			input, err := reader.ReadString('\n')
-			if err != nil {
-				fmt.Println("Error reading")
+			select {
+			case <-closeCh:
 				return
+			default:
+				msg, err := reader.ReadString('.')
+				if err != nil {
+					fmt.Println("Error reading or Server Down:(")
+					os.Exit(0)
+				}
+				fmt.Printf("%s \n> ", msg)
 			}
-			msg := strings.TrimSpace(input)
-			fmt.Println(msg)
 		}
 	}()
 
@@ -34,17 +45,20 @@ func main() {
 
 	for {
 		msg, err := inputReader.ReadString('\n')
-		if err != nil {
-			fmt.Println("Read error ", err)
-			continue
-		}
 
 		msg = strings.TrimSpace(msg)
 
 		_, err = conn.Write([]byte(msg + "\n"))
+
 		if err != nil {
 			fmt.Println("Send Error", err)
 			continue
 		}
+		if msg == "exit" {
+			close(closeCh)
+			wg.Wait()
+			return
+		}
+
 	}
 }

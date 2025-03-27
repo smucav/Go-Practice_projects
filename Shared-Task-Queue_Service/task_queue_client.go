d401320ec4d9db984a8536fd9f202f0f0051cb18
Daniel Tujuma <danielsevani12@gmail.com>
2025-03-27 15:53:37 +0300

    add comments to better understand the code

--- Shared-Task-Queue_Service/task_queue_client.go
+++ Shared-Task-Queue_Service/task_queue_client.go
@@ -10,7 +10,7 @@ import (
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "localhost:8080")
+	conn, err := net.Dial("tcp", "localhost:8080") // connect to the server
 
 	if err != nil {
 		fmt.Println("Connection error: ", err)
@@ -25,29 +25,34 @@ func main() {
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
+		// read from the server
 		reader := bufio.NewReader(conn)
 		for {
+			// used select here to listen to the client exit event and quit gracefully
 			select {
-			case <-closeCh:
+			case <-closeCh: // listen to close channel signal
 				return
 			default:
-				msg, err := reader.ReadString('.')
+				msg, err := reader.ReadString('\n')
 				if err != nil {
 					fmt.Println("Error reading or Server Down:(")
 					os.Exit(0)
 				}
-				fmt.Printf("%s \n> ", msg)
+				// print the message from the server
+				fmt.Printf("%s> ", msg)
 			}
 		}
 	}()
 
+	// read from the client standard in
 	inputReader := bufio.NewReader(os.Stdin)
 
 	for {
+		fmt.Print("> ")
 		msg, err := inputReader.ReadString('\n')
-
+		// remove trailing characters
 		msg = strings.TrimSpace(msg)
-
+		// send to the server
 		_, err = conn.Write([]byte(msg + "\n"))
 
 		if err != nil {
@@ -55,6 +60,7 @@ func main() {
 			continue
 		}
 		if msg == "exit" {
+			// when client exit close the channel then help to signal the channel and exit the loop and go routine
 			close(closeCh)
 			wg.Wait()
 			return
--- Shared-Task-Queue_Service/task_queue_server.go
+++ Shared-Task-Queue_Service/task_queue_server.go
@@ -6,133 +6,190 @@ import (
 	"net"
 	"strings"
 	"sync"
+	"time"
 )
 
-type setTask struct {
-	task string
+// struct each that will be submitted or take with id
+type Task struct {
+	ID      int
+	Content string
 }
 
-type getTask struct{}
-
-type client struct {
-	conn    net.Conn
-	setTask setTask
-	getTask getTask
+// struct that contain task struct and also a response channel
+// to verify whether the task submitted successfully or not
+// think of it as carrier
+type SubmitOp struct {
+	task   Task
+	respCh chan bool
 }
 
+// struct same as SubmitOp this will send the task to the client
+// using a dedicated response channel
+type NextOp struct {
+	respCh chan *Task
+}
+
+// helps to check how many tasks left in the queue
+type StatusOp struct {
+	respCh chan int
+}
+
+// server which governs submit and take tasks from the queue
+// and with waitgroup to help track of the clients
 type TaskQueueServer struct {
-	gets    chan client
-	sets    chan client
-	closeCh chan struct{}
-	wg      sync.WaitGroup
+	submitCh chan SubmitOp
+	nextCh   chan NextOp
+	statusCh chan StatusOp
+	client   sync.WaitGroup
 }
 
+// Initilize the structs
 func NewTaskQueueServer() *TaskQueueServer {
 	return &TaskQueueServer{
-		gets:    make(chan client),
-		sets:    make(chan client),
-		closeCh: make(chan struct{}),
+		submitCh: make(chan SubmitOp),
+		nextCh:   make(chan NextOp),
+		statusCh: make(chan StatusOp),
 	}
 }
 
 func (tqs *TaskQueueServer) Run() {
+	// listen to any connection with this port
 	ln, err := net.Listen("tcp", ":8080")
+
 	if err != nil {
-		fmt.Println("Server Failed")
+		fmt.Println("Server error", err)
 		return
 	}
+	// close listening after done
 	defer ln.Close()
-	fmt.Println("Server started on :8080")
 
-	tqs.wg.Add(1)
-	go func() {
-		defer tqs.wg.Done()
-		for {
-			conn, err := ln.Accept()
-			if err != nil {
-				fmt.Println("Connection error")
-				return
-			}
-			tqs.wg.Add(1)
-			go tqs.handleClient(conn)
-		}
-	}()
+	fmt.Println("Task Queue Server started on :8080")
 
+	// used goroutine here to not block accepting new clients
+	// and to apply stateful goroutine concept
 	go func() {
-		var queue []string
+		// queue where the tasks will be stored
+		var queue []Task
+		// helps to track the id of tasks
+		var taskId int
 		for {
+			// used select here to listen to incoming requests
 			select {
-			case getc := <-tqs.gets:
+			case submit := <-tqs.submitCh:
+				// increase when new task submitted
+				taskId++
+				submit.task.ID = taskId
+				queue = append(queue, submit.task)
+				// send signal that the task is stored successfully
+				submit.respCh <- true
+			case next := <-tqs.nextCh:
+				// return the first task as FIRST IN LAST OUT operation
 				if len(queue) == 0 {
-					fmt.Fprintf(getc.conn, "There is No task have fun.")
+					next.respCh <- nil
 				} else {
-					fmt.Printf("Task %s Taken\n", queue[0])
-					fmt.Fprintf(getc.conn, "Task -> "+queue[0]+".")
+					task := queue[0]
+					next.respCh <- &task
 					queue = queue[1:]
 				}
-
-			case setc := <-tqs.sets:
-				queue = append(queue, setc.setTask.task)
-				fmt.Printf("Task %s Added\n", queue[len(queue)-1])
-				fmt.Fprintf(setc.conn, "Task added successfully.")
-			case <-tqs.closeCh:
-				return
+			case status := <-tqs.statusCh:
+				// returns number of tasks left using channel
+				status.respCh <- len(queue)
 			}
 		}
 	}()
 
-	tqs.wg.Wait()
-	close(tqs.closeCh)
-}
-
-func (tqs *TaskQueueServer) handleClient(conn net.Conn) {
-	defer tqs.wg.Done()
-	defer conn.Close()
-
-	reader := bufio.NewReader(conn)
-	fmt.Fprintf(conn, "Welcome to task queue \nAdd task(Task: task you want to add) or get(NEXT).")
-
 	for {
-		input, err := reader.ReadString('\n')
+		// accept new clients
+		conn, err := ln.Accept()
 		if err != nil {
+			fmt.Println("Server Error", err)
 			return
 		}
 
+		// initilize new go routine for each clients
+		// to handle them concurrently
+		tqs.client.Add(1)
+		go tqs.handleClient(conn)
+	}
+}
+
+func (tqs *TaskQueueServer) handleClient(conn net.Conn) {
+	defer conn.Close()
+	defer tqs.client.Done()
+
+	reader := bufio.NewReader(conn)
+	fmt.Fprintf(conn, "Welcome to Task Queue!\nAdd Task (Task: <task>) or get (NEXT), Status (status), 'exit' to quit:\n")
+
+	for {
+		// accept client message
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Printf("Client %s disconnected\n", conn.RemoteAddr().String())
+			return
+		}
+
+		// remove trailing characters
 		msg := strings.TrimSpace(input)
 
+		// check the message whether they comply with commands avaliable or not
+
 		if msg == "exit" {
+			fmt.Printf("Client %s exited\n", conn.RemoteAddr().String())
 			fmt.Fprintf(conn, "Good bye :)\n")
-			continue
+			return
 		}
 
-		if msg == "NEXT" {
-			get := client{
-				conn:    conn,
-				getTask: getTask{},
-			}
-			tqs.gets <- get
-			continue
-		}
-
-		if strings.HasPrefix(strings.ToLower(msg), "task") {
-			task := strings.TrimSpace(strings.TrimPrefix(strings.ToLower(msg), "task: "))
-			if task == "" {
-				fmt.Fprintf(conn, "Invalid Page\n")
-
+		if strings.ToLower(msg) == "status" {
+			// prepare stat struct and send it to the stat accept channel then process it
+			stat := StatusOp{respCh: make(chan int)}
+			tqs.statusCh <- stat
+			task := <-stat.respCh
+			if task <= 1 {
+				fmt.Fprintf(conn, "%d Task left\n", task)
 				continue
 			}
-			set := client{
-				conn:    conn,
-				setTask: setTask{task: task},
-			}
-			tqs.sets <- set
+			fmt.Fprintf(conn, "%d Tasks left\n", task)
 			continue
 		}
-		fmt.Fprintf(conn, "Unknown command.")
+
+		if strings.ToLower(msg) == "next" {
+			// create a struct with inilized channel to accept the task and return to the client
+			next := NextOp{respCh: make(chan *Task)}
+			tqs.nextCh <- next
+			task := <-next.respCh
+			if task == nil {
+				fmt.Fprintf(conn, "No task left\n")
+				continue
+			}
+			fmt.Fprintf(conn, "task %d: %s\n", task.ID, task.Content)
+			continue
+		}
+
+		if strings.HasPrefix(strings.ToLower(msg), "task: ") {
+			// parse the task name
+			msg = strings.TrimSpace(strings.TrimPrefix(strings.ToLower(msg), "task: "))
+			if msg == "" {
+				fmt.Fprintf(conn, "Invalid Task\n")
+				continue
+			}
+			// create struct with receiver channel and task to send it to the stateful goroutine
+			submit := SubmitOp{task: Task{Content: msg}, respCh: make(chan bool)}
+			tqs.submitCh <- submit
+			if <-submit.respCh {
+				fmt.Fprintf(conn, "Task have been submitted successfully\n")
+				continue
+			}
+		}
+		fmt.Fprintf(conn, "Unknown command: %s\n", msg)
 	}
+
 }
 
 func main() {
 	server := NewTaskQueueServer()
-	server.Run()
+	server.Run() // start the server
+
+	server.client.Wait()
+	fmt.Println("Server Shutting down..")
+	time.Sleep(500 * time.Millisecond) // simulate server shutting down :)
 }
